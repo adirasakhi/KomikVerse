38dc0bd323b1e9ae4991b7dceff4f98b2c151a29
adirasakhi <100334339+adirasakhi@users.noreply.github.com>
2026-02-10 22:40:18 +0700

    Update page.tsx

--- app/genre/page.tsx
+++ app/genre/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import PageHeader from '@/components/PageHeader';
+export const dynamic = 'force-dynamic';
+
 // 1. Update Interface sesuai JSON lo
 interface Genre {
   name: string;
